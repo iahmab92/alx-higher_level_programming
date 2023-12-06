8a8b54a98e39c8d1c5594cffdf61af29b6f69c43
Abraham Idris <ab4excel@gmail.com>
2023-12-06 05:48:40 -0800

    print last digit of a number

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -8,6 +8,6 @@ print("Last digit of {} is {} and is ".format(number, lastdigit), end="")
 if lastdigit > 5:
     print('greater than 5')
 elif lastdigit == 0:
-    print('is 0')
+    print('0')
 else:
     print('less than 6 and not 0')
