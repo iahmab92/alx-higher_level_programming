2397e7ba5b677cafe0e4df5dfc5d44ea539f765c
Abraham Idris <ab4excel@gmail.com>
2023-12-27 09:24:33 -0800

    square class with a size attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Defines a class Square"""
+
+
+class Square:
+    """
+    Class that defines properties of square based on 0-square.py
+
+    Attributes:
+        size: size of 1 side of a square
+    """
+    def __init__(self, size):
+        """Creates new instance of square
+
+        Args:
+            size: size of the square
+        """
+        self.size = size
