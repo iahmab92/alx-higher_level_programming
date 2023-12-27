a392a82e94aa219faeb4dd8461d386545d0d726d
Abraham Idris <ab4excel@gmail.com>
2023-12-27 09:47:40 -0800

    task 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -17,7 +17,7 @@ class Square:
         """
         self.__size = size
 
-        if type(size) is not int:
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be  >= 0")
