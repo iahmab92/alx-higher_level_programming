6be3bd5774d73ef505bdbac9979570ec664c93d9
Abraham Idris <ab4excel@gmail.com>
2023-12-27 09:40:28 -0800

    task 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Defines a class Square"""
+
+
+class Square:
+    """
+    Class that defines properties of square by: (based on 1-square.py).
+
+    Attributes:
+        size: size of a square (1 side).
+    """
+    def __init__(self, size=0):
+        """Creates new instances of square (1 side).
+
+        Args:
+            size: size of the square.
+        """
+        self.__size = size
+
+        if size % 2 != 0:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueErro("size must be  >= 0")
+        else:
+            self.__size = size
