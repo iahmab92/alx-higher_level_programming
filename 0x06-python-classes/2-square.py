cf3fb1ada216671c22f1e17d2bcb2aa7a6671e27
Abraham Idris <ab4excel@gmail.com>
2023-12-27 09:51:07 -0800

    task 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -10,14 +10,14 @@ class Square:
         size: size of a square (1 side).
     """
     def __init__(self, size=0):
-        """Creates new instances of square (1 side).
+        """Creates new instances of square.
 
         Args:
-            size: size of the square.
+            size: size of the square (1 side).
         """
         self.__size = size
 
-        if not isinstance(size, int):
+        if type(size) is not int:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be  >= 0")
