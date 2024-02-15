a835edccdea33a3f1c720d0fd7319af387a52794
Abraham Idris <ab4excel@gmail.com>
2024-02-15 11:11:01 -0800

    create a db and a tablr

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,7 +1,7 @@
 -- create a table and a database
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states (
-	id INT NOT NULL  AUTO_INCREMENT,
-	name VARCHAR(256) NOT NULL,
-	PRIMARY KEY(`id`)
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
+CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`states` (
+    PRIMARY KEY(`id`),
+    `id`   INT          NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(256) NOT NULL
 );
